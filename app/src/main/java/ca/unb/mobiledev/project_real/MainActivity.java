bbd2d54b8974877bf721b6613f7ce029ac4b543c
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2022-04-11 14:39:28 -0300

    this timer muthafucker be pisssing mre off

--- app/src/main/java/ca/unb/mobiledev/project_real/MainActivity.java
+++ app/src/main/java/ca/unb/mobiledev/project_real/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.task_list_page);
+        setContentView(R.layout.landing_page);
     }
 }
--- app/src/main/java/ca/unb/mobiledev/project_real/activities/TimerActivity.java
+++ app/src/main/java/ca/unb/mobiledev/project_real/activities/TimerActivity.java
@@ -1,4 +1,15 @@
 package ca.unb.mobiledev.project_real.activities;
 
-public class TimerActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ca.unb.mobiledev.project_real.R;
+
+public class TimerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.timer_layout);
+    }
 }
