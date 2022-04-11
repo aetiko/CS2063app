af2195f3ec583fb4deb7d964684c9d2ac0e4985f
gibarraUNB3035 <gibarra@unb.ca>
2022-04-11 18:55:21 -0300

    list page fixed

--- app/src/main/java/ca/unb/mobiledev/project_real/activities/TaskListActivity.java
+++ app/src/main/java/ca/unb/mobiledev/project_real/activities/TaskListActivity.java
@@ -1,17 +1,25 @@
 package ca.unb.mobiledev.project_real.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import ca.unb.mobiledev.project_real.R;
@@ -19,67 +27,200 @@ import ca.unb.mobiledev.project_real.adapters.TaskAdapter;
 import ca.unb.mobiledev.project_real.model.Task;
 
 public class TaskListActivity extends AppCompatActivity {
+    Parcelable state;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    ArrayList<Task> taskList;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    private static Bundle mBundleRecyclerViewState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_list_page);
+        Log.i("TaskList", "in create");
 
 
-        ArrayList<Task> taskList = generateTasks();
-        Log.i("Main Activity", taskList.get(0).getName().toString());
+        sharedPreferences = getSharedPreferences("", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
-        TaskAdapter adapter = new TaskAdapter(taskList, this);
+        loadData();
 
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+//        editor.putInt("taskCounter", 0);
+//        editor.apply();
+
+
+            Task newTask = (Task)getIntent().getSerializableExtra("new task");
+
+
+
+            if(newTask!=null){
+                Log.i("TaskList", "Category: "+newTask.getCategory()+"");
+                Log.i("TaskList", "Id: "+newTask.getId()+"");
+                Log.i("TaskList", "Name: "+newTask.getName()+"");
+                taskList.add(newTask);
+            }
+
+        TaskAdapter adapter = new TaskAdapter(taskList, this, sharedPreferences);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.setAdapter(adapter);
 
+        FloatingActionButton floatingActionButton = findViewById(R.id.add_task_btn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                Intent intent = new Intent(TaskListActivity.this, CreateTaskPage.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
-    public ArrayList<Task> generateTasks(){
-        Task.Builder taskBuilder;
-        Task taskOne;
-        Task taskTwo;
-        Task taskThree;
-        Task taskFive;
-        Task taskSix;
+//        @Override
+//    protected void onRestoreInstanceState( Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//
+//        if(savedInstanceState!=null){
+//            Log.i("TaskList", "not null");
+//        }
+//        else{
+//            Log.i("TaskList", "null");
+//        }
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle savedStateInstance) {
+//        super.onSaveInstanceState(savedStateInstance);
+//        Log.i("TaskList", "saving");
+//        saveData(savedStateInstance);
+//    }
 
-        ArrayList<Task> list = new ArrayList<Task>();
 
-        ImageView oneImage = new ImageView(this);
-        oneImage.setImageResource(R.drawable.ic_vector);
-        ImageView twoImage = new ImageView(this);
-        twoImage.setImageResource(R.drawable.ic_vector);
-        ImageView threeImage = new ImageView(this);
-        threeImage.setImageResource(R.drawable.ic_vector);
+    private void saveData() {
+        // method for saving the data in array list.
+        // creating a variable for storing data in
+        // shared preferences.
+        SharedPreferences sharedPreferences = getSharedPreferences("", MODE_PRIVATE);
 
-        taskBuilder = new Task.Builder("Do CS2026 Project",'e' );
-        taskOne = taskBuilder.build();
-        taskBuilder = new Task.Builder("Finish Song",'c' );
-        taskTwo = taskBuilder.build();
-        taskBuilder = new Task.Builder("Smoke Weed",'w' );
-        taskThree = taskBuilder.build();
+        // creating a variable for editor to
+        // store data in shared preferences.
+        SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        taskBuilder = new Task.Builder("Smoke Weed",'r' );
-        taskFive = taskBuilder.build();
-        taskBuilder = new Task.Builder("Smoke Weed",'h' );
-        taskSix = taskBuilder.build();
+        // creating a new variable for gson.
+        Gson gson = new Gson();
 
-        list.add(taskOne);
-        list.add(taskTwo);
-        list.add(taskThree);
-        list.add(taskFive);
-        list.add(taskSix);
+        // getting data from gson and storing it in a string.
+        String json = gson.toJson(taskList);
 
-        return list;
+        // below line is to save data in shared
+        // prefs in the form of string.
+        editor.putString("courses", json);
+
+        // below line is to apply changes
+        // and save data in shared prefs.
+        editor.apply();
+
+        // after saving data we are displaying a toast message.
+//        Toast.makeText(this, "Saved Array List to Shared preferences. ", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadData() {
+        // method to load arraylist from shared prefs
+        // initializing our shared prefs with name as
+        // shared preferences.
+        SharedPreferences sharedPreferences = getSharedPreferences("", MODE_PRIVATE);
+
+        // creating a variable for gson.
+        Gson gson = new Gson();
+
+        // below line is to get to string present from our
+        // shared prefs if not present setting it as null.
+        String json = sharedPreferences.getString("courses", null);
+
+        // below line is to get the type of our array list.
+        Type type = new TypeToken<ArrayList<Task>>() {}.getType();
+
+        // in below line we are getting data from gson
+        // and saving it to our array list
+        taskList = gson.fromJson(json, type);
+
+        // checking below if the array list is empty or not
+        if (taskList == null) {
+            // if the array list is empty
+            // creating a new array list.
+            taskList = new ArrayList<>();
+        }
     }
 
 
-    public void cardClick(View v)
-    {
-        Intent intent = new Intent(TaskListActivity.this, TimerActivity.class);
-        startActivity(intent);
-    }
+//
+//   public ArrayList<Task> generateTasks(){
+//        Task.Builder taskBuilder;
+//        Task taskOne;
+//        Task taskTwo;
+//        Task taskThree;
+//        Task taskFive;
+//        Task taskSix;
+//
+//        ArrayList<Task> list = new ArrayList<Task>();
+//
+//        ImageView oneImage = new ImageView(this);
+//        oneImage.setImageResource(R.drawable.ic_vector);
+//        ImageView twoImage = new ImageView(this);
+//        twoImage.setImageResource(R.drawable.ic_vector);
+//        ImageView threeImage = new ImageView(this);
+//        threeImage.setImageResource(R.drawable.ic_vector);
+//
+//        taskBuilder = new Task.Builder("Do CS2026 Project",'e' );
+//        taskOne = taskBuilder.build();
+//        taskBuilder = new Task.Builder("Finish Song",'c' );
+//        taskTwo = taskBuilder.build();
+//        taskBuilder = new Task.Builder("Smoke Weed",'w' );
+//        taskThree = taskBuilder.build();
+//
+//        taskBuilder = new Task.Builder("Smoke Weed",'r' );
+//        taskFive = taskBuilder.build();
+//        taskBuilder = new Task.Builder("Smoke Weed",'h' );
+//        taskSix = taskBuilder.build();
+//
+//        list.add(taskOne);
+//        list.add(taskTwo);
+//        list.add(taskThree);
+//        list.add(taskFive);
+//        list.add(taskSix);
+//
+//        return list;
+//    }
 
+    //    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        Parcelable listState = recyclerView.getLayoutManager().onSaveInstanceState();
+//
+//        for(int i = 0; i < )
+//        // putting recyclerview position
+//        outState.putParcelable("state", listState);
+//        // putting recyclerview items
+//        outState.putParcelableArrayList("list",taskList);
+//        super.onSaveInstanceState(outState);
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        state = recyclerView.getLayoutManager().onSaveInstanceState();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        recyclerView.getLayoutManager().onRestoreInstanceState(state);
+//    }
 }
